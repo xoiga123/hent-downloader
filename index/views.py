964174bf97e4eb665915f14f269f7dbbaa998ef2
Xoi Ga <xoiga123@outlook.com.vn>
2021-11-23 17:15:34 +0700

    add multithread

--- index/views.py
+++ index/views.py
@@ -7,48 +7,54 @@ from io import BytesIO
 import re
 import os
 import time
+from threading import Thread
 
 
 def download(link):
     # Adding Browser / User-Agent Filtering should help ie.
     # will give you only desktop firefox User-Agents on Windows
     scraper = cloudscraper.create_scraper(browser={'browser': 'firefox','platform': 'windows','mobile': False})
-
-    html = scraper.get(link).content
-
     name = re.search("manga/(.*?)/", link).group(1)
-    soup = BeautifulSoup(html, 'html.parser')
-    img_list = []
+    try:
+        chapter = re.search("manga/.*?/(.*?)/", link).group(1)
+    except:
+        chapter = None
 
-    chapters = soup.find_all("li", {"class": "wp-manga-chapter"})
-    chapter_name = None
-    if len(chapters) != 0:
-        # multi download mode
-        for chapter in chapters[::-1]:
-            link = chapter.find('a')['href']
-            chapter_html = scraper.get(link, headers={'referer': "https://hentaicube.net/"}).content
-            chapter_soup = BeautifulSoup(chapter_html, 'html.parser')
-            for noscript in chapter_soup.find_all("noscript"):
-                img = noscript.find("img")
-                if not img: continue
-
-                link = img["src"]
-                img_list.append(
-                    Image.open(BytesIO(scraper.get(link, headers={'referer': "https://hentaicube.net/"}).content)))
+    if chapter:
+        # single download
+        img_list = [[]]
+        crawl_chapter(scraper, link, img_list, 0)
     else:
-        # single download mode
-        chapter_name = soup.find("div", {'id': "manga-reading-nav-head"})['data-chapter']
-        for noscript in soup.find_all("noscript"):
-            img = noscript.find("img")
-            if not img: continue
+        # multi download
+        html = scraper.get(link).content
+        soup = BeautifulSoup(html, 'html.parser')
+        chapters = soup.find_all("li", {"class": "wp-manga-chapter"})
+        img_list = [[] for _ in range(len(chapters))]
+        threads = []
+        for index, chap in enumerate(chapters[::-1]):
+            link = chap.find('a')['href']
+            process = Thread(target=crawl_chapter, args=[scraper, link, img_list, index])
+            process.start()
+            threads.append(process)
+        for process in threads:
+            process.join()
 
-            link = img["src"]
-            img_list.append(
-                Image.open(BytesIO(scraper.get(link, headers={'referer': "https://hentaicube.net/"}).content)))
-
-    pdf_filename = "{}-{}.pdf".format(name, chapter_name if chapter_name else "%schaps" % len(chapters))
-    img_list[0].save(pdf_filename, "PDF", resolution=300.0, save_all=True, append_images=img_list[1:])
+    img_list_flatten = [item for sublist in img_list for item in sublist]
+    pdf_filename = "{}-{}.pdf".format(name, chapter if chapter else "%schaps" % len(chapters))
+    img_list_flatten[0].save(pdf_filename, "PDF", resolution=300.0, save_all=True, append_images=img_list_flatten[1:])
     return pdf_filename
+
+
+def crawl_chapter(scraper, link, img_list, index):
+    html = scraper.get(link).content
+    soup = BeautifulSoup(html, 'html.parser')
+    for noscript in soup.find_all("noscript"):
+        img = noscript.find("img")
+        if not img: continue
+
+        link = img["src"]
+        img_list[index].append(
+            Image.open(BytesIO(scraper.get(link, headers={'referer': "https://hentaicube.net/"}).content)))
 
 
 # Create your views here.
